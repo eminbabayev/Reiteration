77a55053e8898e23d175d8243b2273b6daddc238
emin <emin_babayev99@mail.ru>
2018-07-19 11:38:26 +0400

    повторение javascript-algoritms,arrays,functions,loops,objects,operators

---
+++ 19.07.2018/script.js
@@ -0,0 +1,120 @@
+﻿/*var x = prompt("Пожалуйста,введите число");
+if(x%2==0){
+    console.log(x-x);
+}
+else{
+    console.log(x*x);
+}*/
+
+
+
+
+/*var x = 100;
+console.log(typeof(x));
+
+var world = "Hello,world!";
+console.log(typeof(world));
+
+var isMarried = false;
+console.log(typeof(isMarried));*/
+
+
+
+
+/*var x;
+x=3;
+
+var y;
+y=4;
+
+var z;
+
+console.log(z = x + y);
+
+console.log(z = x - y);
+
+console.log( z = (x%2)+y);*/
+
+/*var x = prompt("Введите число: ");
+
+if(x%3==0 && x%7==0){
+    console.log(x+ " делиться и на 3 и на 7");
+}
+
+else if (x%3==0){
+    console.log(x+ " делиться только на 3");
+}
+
+else if(x%7==0){
+    console.log(x+ " делиться только на 7");
+}
+else{
+    console.log(x+ " не делиться на 3 и 7");
+}*/
+
+
+
+
+/*var i =1;
+while(i<10){
+    console.log("ПРИВЕТ,",i);
+    i++;
+}*/
+
+
+
+
+/*var x = 100;
+console.log(typeof(x));
+
+var world = "Hello,world!";
+console.log(typeof(world));
+
+var isMarried = false;
+console.log(typeof(isMarried));*/
+
+
+
+/*var n =11,  m =20,  total = 0;
+while(n<=m){
+    total +=n;
+    n++;
+    console.log(n,m,total);
+}*/
+
+
+
+
+/*var x = 100;
+console.log(typeof(x));
+
+var world = "Hello,world!";
+console.log(typeof(world));
+
+var isMarried = false;
+console.log(typeof(isMarried));*/
+
+
+
+
+/*function Loops (number1,number2){
+    total = 0;
+    i = 0;
+    while(i<10){
+        total= total +(number1-number2);
+        i++;
+    }
+   
+    
+}
+
+var a = Loops(1333,462);
+console.log(a);*/
+
+
+var yaslar = [30,25,18,21];
+var i = 0;
+while(i<yaslar.length){
+    console.log(yaslar[i]);
+    i++;
+}
